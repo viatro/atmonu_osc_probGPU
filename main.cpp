dab26376284161248a9fac34b827d1566e28c773
viatro <vi-at@ya.ru>
2016-09-20 08:02:20 +0300

    main.[cc -> cpp]

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ using namespace std;
 const double Pi = acos(-1.0);
 
 class EarthCrossing {
-    
+
   private:
-    
+
     double x;
     double xini;
     double xend;
@@ -19,22 +19,22 @@ class EarthCrossing {
     double zenith_angle;
     double production_height;
     double detector_depth;
-    
+
     const size_t R = 6371000; // Earth radius in meters
     const size_t hAtm = 86000; // atmosphere height in meters
     map<long, double> earth_layers_density_map;
     vector<array<double, 3>> crossing_layers;
-    
+
     void FillLayersDensities(size_t dr = 100) {
         map<int, array<double, 3> > earth_density_map;
         map<double, array<double, 3> > atmo_temperature_map;
         map<int, double> ye_map;
-        
+
         ye_map[1221500] = 0.4656;
         ye_map[3480000] = 0.4656;
         ye_map[     R ] = 0.4957;
         ye_map[   2*R ] = 0.5   ;
-        
+
         // r in meters, a[0] + a[1]*r + a[2]*r^2 density in kg/m^3
         earth_density_map[1221500] = {1.3088e4,  1.9110e-8, -2.1773e-10};
         earth_density_map[3480000] = {1.2346e4,  1.3976e-4, -2.4123e-10};
@@ -47,7 +47,7 @@ class EarthCrossing {
         earth_density_map[6356000] = {2.9000e3,  0.       ,  0.        };
         earth_density_map[6368000] = {2.6000e3,  0.       ,  0.        };
         earth_density_map[6371000] = {1.0200e3,  0.       ,  0.        };
-        
+
         // geopotential h in meters, T in Kelvin, T lapse in K/m, density in g/cm^3
         atmo_temperature_map[    0.           ] = { 15.0  + 273.15, -0.0065, 1.2250e-3           };
         atmo_temperature_map[11000.           ] = {-56.5  + 273.15,  0.0   , 3.639271988833481e-4};
@@ -57,7 +57,7 @@ class EarthCrossing {
         atmo_temperature_map[51000.           ] = {- 2.5  + 273.15, -0.0028, 8.61479984576618e-7 };
         atmo_temperature_map[71000.           ] = {-58.5  + 273.15, -0.002 , 6.420472983251111e-8};
         atmo_temperature_map[84854.57642868205] = {-86.28 + 273.15,  0.0   , 6.954393506100748e-9};
-        
+
         double rho = 0.0;
         for (size_t r = 0; r <= R + hAtm; r += dr) {
             if (r <= R) {
@@ -66,19 +66,19 @@ class EarthCrossing {
             } else if (r > R && r <= R + hAtm) {
                 double h = r - R;
                 h = h*R/(h + R); // convertion from geometric to geopotential
-                
+
                 auto iter = atmo_temperature_map.lower_bound(h);
                 --iter;
-                
+
                 double g0 = 9.80665; // m/s^2
                 double R = 8.3144598; // N·m/(mol·K)
                 double M = 0.0289644; // kg/mol
-                
+
                 double hb   = iter->first;
                 double Tb   = iter->second[0];
                 double Lb   = iter->second[1];
                 double rhob = iter->second[2];
-                
+
                 if (Lb == 0.0) {
                     rho = rhob * exp(-g0*M*(h - hb)/(R*Tb));
                 } else {
@@ -88,14 +88,14 @@ class EarthCrossing {
             earth_layers_density_map[r] = rho;
         }
     }
-    
+
     double GetYe(double r) {
         //if (r <= 1221500) return 0.4656;
         if (r <= 3480000) return 0.4656;
         if (r <= R      ) return 0.4957;
         else              return 0.5   ;
     }
-    
+
     void FillCrossingLayers() {
         crossing_layers.clear();
         double rmin = R - detector_depth;
@@ -157,7 +157,7 @@ class EarthCrossing {
             double ylow = xlow * k + b;
             double xup = 0.;
             double yup = 0.;
-            
+
             while (r > rmin) {
                 xup = xlow;
                 yup = ylow;
@@ -173,7 +173,7 @@ class EarthCrossing {
                 length = hypot(xup - xlow, yup - ylow);
                 crossing_layers.emplace_back(array<double, 3>{length, rho, ye});
             }
-            
+
             if (zenith_angle > Pi/2) {
                 double rmax = R - detector_depth;
                 while (r < rmax) {
@@ -193,72 +193,94 @@ class EarthCrossing {
             }
         }
     }
-    
+
   public:
-    
+
     EarthCrossing(double zenith_angle = 0.0, double production_height = 0.0, double detector_depth = 0.0) {
         FillLayersDensities(100);
         SetParams(zenith_angle, production_height, detector_depth);
     }
-    
+
     ~EarthCrossing(){}
-    
+
     void SetParams(double zenith_angle = 0.0, double production_height = 0.0, double detector_depth = 0.0) {
         this->zenith_angle = zenith_angle;
         this->production_height = production_height;
         this->detector_depth = detector_depth;
-        
+
         double b = R - detector_depth;
         double c = R + production_height;
-        
+
         double a1 = b * cos(Pi - zenith_angle);
         double f = b * sin(Pi - zenith_angle);
         double a2 = sqrt(c*c - f*f);
         L = a1 + a2;
-        
+
         FillCrossingLayers();
     }
-    
+
     const vector<array<double, 3>>& GetCrossingLayers() const {
         return crossing_layers;
     }
-    
+
 };
 
-//extern "C" void setMNS(double x12, double x13, double x23, double m21, double m23, double Delta = 0.0, bool kSquared = true);
-//extern "C" void GetProb(int Alpha, int Beta, double Path, double Density, double ye, double *Energy, int n, double *oscw);
+extern /*"C"*/ void setMNS(float x12, float x13, float x23, float m21, float m23, float Delta = 0.0, bool kSquared = true);
+extern /*"C"*/ void GetProb(int Alpha, int Beta, float Path, float Density, float ye, float *Energy, int n, float *oscw);
 
 int main (int argc, char **argv) {
-    
-    EarthCrossing earth_crossing(0, 0);
-    size_t total = 0;
-    for (size_t i = 0; i <= 180*60; ++i) {
-        earth_crossing.SetParams(i*Pi/180, 15000.);
-        total += earth_crossing.GetCrossingLayers().size();
-        if ((i + 1) % 60 == 0) cout << "\r" << double(i)/(180*60+1)*100. << "          " << flush;
-    }
-    cout << "\ntotal: " << total << endl;
 
-    /*setMNS(0.304, 0.0219, 0.514, 7.53e-5, 2.42e-3);
+    setMNS(0.304, 0.0219, 0.514, 7.53e-5, 2.42e-3);
 
-    int points_per_decade = 100000;
+    int points_per_decade = 10000;
 
-    vector<double> energies;
+    vector<float> energies;
     for (long i = -6*points_per_decade; i <= 1*points_per_decade; ++i) {
         energies.push_back(pow10(i/double(points_per_decade)));
     }
 
-    vector<vector<double>> osc_weights(4, vector<double>(energies.size(), 0.0));
+    vector<vector<float>> osc_weights(3, vector<float>(energies.size(), 0.0));
+    auto oscw_tmp = oscw_tmp_sum = osc_weights;
 
-    GetProb(2, 1, 2.*6371., 13.0, 0.5, &energies[0], energies.size(), &osc_weights[1][0]);
-    GetProb(2, 2, 2.*6371., 13.0, 0.5, &energies[0], energies.size(), &osc_weights[2][0]);
-    GetProb(2, 3, 2.*6371., 13.0, 0.5, &energies[0], energies.size(), &osc_weights[3][0]);
+
+    EarthCrossing earth_crossing(Pi*0.75, 15000.);
+    auto layers = earth_crossing.GetCrossingLayers();
+
+    GetProb(1, 1, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), osc_weights[0].data());
+    GetProb(1, 2, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), osc_weights[1].data());
+    GetProb(1, 3, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), osc_weights[2].data());
+
+    for (size_t iLayer = 1; iLayer < layers.size(); ++iLayer) {
+        for (int nuFlavorIn = 0; nuFlavorIn < 3; ++nuFlavorIn) {
+            for (int nuFlavorOut = 0; nuFlavorOut < 3; ++nuFlavorOut) {
+                GetProb(nuFlavorIn+1, nuFlavorOut+1, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), oscw_tmp[nuFlavorOut].data());
+                GetProb(nuFlavorIn+1, nuFlavorOut+1, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), oscw_tmp[nuFlavorOut].data());
+                GetProb(nuFlavorIn+1, nuFlavorOut+1, layers[0][0], layers[0][1], layers[0][2], energies.data(), energies.size(), oscw_tmp[nuFlavorOut].data());
+
+                for (size_t i = 0; i < energies.size(); ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        if (oscw_tmp[j][i] < 0.0 || oscw_tmp[j][i] > 1.0 || oscw_tmp[j][i] != oscw_tmp[j][i]) {
+                            cout << energies[i]*1000.0 << " MeV :\t" << j << "\t" << oscw_tmp[j][i] << endl;
+                        }
+                    }
+
+                }
+            }
+        }
+        cout << "\r" << iLayer << " / " << layers.size() << flush;
+
+    }
+    cout << endl;
+
+    /*GetProb(1, 1, 2.*6371., 13.0, 0.5, energies.data(), energies.size(), osc_weights[0].data());
+    GetProb(1, 2, 2.*6371., 13.0, 0.5, energies.data(), energies.size(), osc_weights[1].data());
+    GetProb(1, 3, 2.*6371., 13.0, 0.5, energies.data(), energies.size(), osc_weights[2].data());
 
     for (size_t i = 0; i < energies.size(); ++i) {
-        for (int j = 1; j <= 3; ++j) {
-            //if (osc_weights[j][i] < 0.0 || osc_weights[j][i] > 1.0) {
+        for (int j = 0; j < 3; ++j) {
+//            if (osc_weights[j][i] < 0.0 || osc_weights[j][i] > 1.0) {
                 cout << energies[i]*1000.0 << " MeV :\t" << j << "\t" << osc_weights[j][i] << endl;
-            //}
+//            }
         }
 
     }*/
